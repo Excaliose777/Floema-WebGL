66031a5172c9d5fc035a2b9836f9b1f88221b70a
AlaminxAb <alaminxab@gmail.com>
2023-07-25 16:42:20 +0100

    Implementing Dragable Elements

--- app/components/Canvas/Media.js
+++ app/components/Canvas/Media.js
@@ -4,12 +4,13 @@ import fragment from 'shaders/plane-fragment.glsl'
 import vertex from 'shaders/plane-vertex.glsl'
 
 export default class {
-  constructor({ element, geometry, gl, index, scene }) {
+  constructor({ element, geometry, gl, index, scene, sizes }) {
     this.element = element
     this.geometry = geometry
     this.gl = gl
     this.index = index
     this.scene = scene
+    this.sizes = sizes
 
     this.createTexture()
     this.createProgram()
@@ -19,7 +20,7 @@ export default class {
   createTexture() {
     this.texture = new Texture(this.gl)
 
-    console.log(this.element)
+    // console.log(this.element)
 
     this.image = new window.Image()
     this.image.crossOrigin = 'anonymous'
@@ -47,7 +48,60 @@ export default class {
   
     this.mesh.setParent(this.scene)
 
-    this.mesh.position.x += this.index * this.mesh.scale.x
+    this.mesh.scale.x = 2
+  }
+
+  createBounds({ sizes }) {
+    this.sizes = sizes
+
+    this.bounds = this.element.getBoundingClientRect()
+
+    this.updateScale()
+    this.updateX()
+    this.updateY()
+
+    // console.log(this.bounds)
+  }
+
+    /**EVENTS */
+  onResize(sizes) {
+    this.createBounds(sizes)
+  }
+
+  /**
+   * UPDATE 
+  */
+  updateScale() {
+    this.height = this.bounds.height / window.innerHeight
+    this.width = this.bounds.width / window.innerWidth
+
+    this.mesh.scale.x = this.sizes.width * this.width
+    this.mesh.scale.y = this.sizes.height * this.height
+
+    
+    
+
+
+    // console.log(this.width, this.height)
+  }
+
+  updateX(x = 0){
+    this.x = (this.bounds.left + x) / window.innerWidth
+
+    this.mesh.position.x = (-this.sizes.width / 2) + (this.mesh.scale.x / 2) + (this.x * this.sizes.width)
+  }
+
+  updateY(y = 0){
+    this.y = (this.bounds.top + y) / window.innerHeight
+
+    this.mesh.position.y = (this.sizes.height / 2) - (this.mesh.scale.y / 2) - (this.y * this.sizes.height)
+  }
+
+  update(scroll){
+    if(!this.bounds) return
+    
+    this.updateX(scroll.x)
+    this.updateY(scroll.y)
   }
 
 }
