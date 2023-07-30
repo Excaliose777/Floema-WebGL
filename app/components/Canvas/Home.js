4a5ba5802945659edcedca3b6da0f29a55c6a8be
AlaminxAb <alaminxab@gmail.com>
2023-07-30 22:33:37 +0100

    Implementing Infinite images logic

--- app/components/Canvas/Home.js
+++ app/components/Canvas/Home.js
@@ -1,49 +1,52 @@
-import map from 'lodash/map'
-import Media from './Media'
-import { Plane, Transform } from 'ogl'
-import gsap from 'gsap'
+import map from "lodash/map";
+import Media from "./Media";
+import { Plane, Transform } from "ogl";
+import gsap from "gsap";
 
-import { lerp } from 'utils/math'
+import { lerp } from "utils/math";
 
 export default class {
   constructor({ gl, scene, sizes }) {
-    this.gl = gl
-    this.scene = scene
-    this.sizes = sizes
-    this.group = new Transform()
+    this.gl = gl;
+    this.scene = scene;
+    this.sizes = sizes;
+    this.group = new Transform();
 
-    this.mediasElements = document.querySelectorAll('.home__gallery__media__image')
+    this.galleryElement = document.querySelector(".home__gallery");
+    this.mediasElements = document.querySelectorAll(
+      ".home__gallery__media__image"
+    );
 
-    this.createGeometry()
-    this.createGallery()
+    this.createGeometry();
+    this.createGallery();
 
-    this.group.setParent(scene)
+    this.group.setParent(scene);
 
     this.x = {
-      current:0,
-      target:0,
-      lerp:0.1
-    }
+      current: 0,
+      target: 0,
+      lerp: 0.1,
+    };
 
     this.y = {
-      current:0,
-      target:0,
-      lerp:0.1
-    }
+      current: 0,
+      target: 0,
+      lerp: 0.1,
+    };
 
     this.scrollCurrent = {
-      x:0,
-      y:0
-    }
+      x: 0,
+      y: 0,
+    };
 
     this.scroll = {
-      x:0,
-      y:0
-    }
+      x: 0,
+      y: 0,
+    };
   }
 
   createGeometry() {
-    this.geometry = new Plane(this.gl)
+    this.geometry = new Plane(this.gl);
   }
 
   createGallery() {
@@ -54,50 +57,112 @@ export default class {
         index,
         gl: this.gl,
         scene: this.group,
-        sizes: this.sizes
-      })
-    })
+        sizes: this.sizes,
+      });
+    });
   }
 
   /**
-   * 
+   *
    * EVENTS
    */
 
-
   onResize(event) {
-    map(this.medias, media => media.onResize(event))
+    map(this.medias, (media) => media.onResize(event));
+
+    this.galleryBounds = this.galleryElement.getBoundingClientRect();
+
+    this.sizes = event.sizes;
+
+    this.gallerySizes = {
+      height: this.galleryBounds.height / window.innerHeight * this.sizes.height,
+      width: this.galleryBounds.width / window.innerWidth * this.sizes.width
+
+    }
   }
 
-  onTouchDown({x,y}){
-    this.scrollCurrent.x = this.scroll.x
-    this.scrollCurrent.y = this.scroll.y
+  onTouchDown({ x, y }) {
+    this.scrollCurrent.x = this.scroll.x;
+    this.scrollCurrent.y = this.scroll.y;
   }
 
-  onTouchMove({x,y}){
-    const xDistance = x.start - x.end
-    const yDistance = y.start - y.end
+  onTouchMove({ x, y }) {
+    const xDistance = x.start - x.end;
+    const yDistance = y.start - y.end;
 
-    this.x.target = this.scrollCurrent.x - xDistance
-    this.y.target = this.scrollCurrent.y - yDistance
+    this.x.target = this.scrollCurrent.x - xDistance;
+    this.y.target = this.scrollCurrent.y - yDistance;
   }
 
-  onTouchUp({x,y}){
-
-  }
+  onTouchUp({ x, y }) {}
 
   /**
    * UPDATE
    */
   update() {
-    this.x.current = gsap.utils.interpolate(this.x.current, this.x.target, this.x.lerp)
-    this.y.current = gsap.utils.interpolate(this.y.current, this.y.target, this.y.lerp)
+    if (!this.galleryBounds) return;
 
-    this.scroll.x = this.x.current
-    this.scroll.y = this.y.current
+    this.x.current = gsap.utils.interpolate(
+      this.x.current,
+      this.x.target,
+      this.x.lerp
+    );
+    this.y.current = gsap.utils.interpolate(
+      this.y.current,
+      this.y.target,
+      this.y.lerp
+    );
 
-    map(this.medias, media => {
-      media.update(this.scroll)
-    })
+    if (this.scroll.x < this.x.current) {
+      this.x.direction = "right";
+    } else if (this.scroll.x > this.x.current) {
+      this.x.direction = "left";
+    }
+
+    if (this.scroll.y < this.y.current) {
+      this.y.direction = "top";
+    } else if (this.scroll.y > this.y.current) {
+      this.y.direction = "bottom";
+    }
+
+
+    this.scroll.x = this.x.current;
+    this.scroll.y = this.y.current;
+
+    map(this.medias, (media, index) => {
+      const scaleX = media.mesh.scale.x / 2;
+
+      if (this.x.direction === "left") {
+        const x = media.mesh.position.x + scaleX
+
+        if (x < -this.sizes.width / 2) {
+          media.extra.x += this.gallerySizes.width;
+        }
+      } else if (this.x.direction === "right") {
+        const x = media.mesh.position.x - scaleX
+
+        if (x > this.sizes.width / 2) {
+          media.extra.x -= this.gallerySizes.width;
+        }
+      }
+
+      const scaleY = media.mesh.scale.y / 2;
+
+      if (this.y.direction === "top") {
+        const y = media.mesh.position.y + scaleY
+
+        if (y < -this.sizes.height / 2) {
+          media.extra.y += this.gallerySizes.height;
+        }
+      } else if (this.y.direction === "bottom") {
+        const y = media.mesh.position.y - scaleY
+
+        if (y > this.sizes.height / 2) {
+          media.extra.y -= this.gallerySizes.height;
+        }
+      }
+
+      media.update(this.scroll);
+    });
   }
 }
